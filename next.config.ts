7537ab50794915c24edae655c85f2a309c2b7646
John <jpaappiah@gmail.com>
2026-08-10 18:47:38 +0000

    Include Tesseract dependencies in Vercel trace

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,13 @@ const nextConfig: NextConfig = {
     "/api/timetable/extract": [
       "./node_modules/tesseract.js/**/*",
       "./node_modules/tesseract.js-core/**/*",
+      "./node_modules/bmp-js/**/*",
+      "./node_modules/idb-keyval/**/*",
+      "./node_modules/is-url/**/*",
+      "./node_modules/node-fetch/**/*",
+      "./node_modules/regenerator-runtime/**/*",
+      "./node_modules/wasm-feature-detect/**/*",
+      "./node_modules/zlibjs/**/*",
     ],
   },
   async headers() {
